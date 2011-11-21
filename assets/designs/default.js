3b2327a19fc25ee6a5b5db3e20f19fe6c45e626b
Damon Oehlman <damon.oehlman@gmail.com>
2011-11-21 23:44:36 +1000

    Updated apps to use `app::` prefix

--- assets/designs/default.js
+++ assets/designs/default.js
@@ -4,32 +4,39 @@ function mapApps(doc) {
         reLib = new RegExp('^(lib|node_modules|' + syncPaths + ')\/', 'i'),
         ignoreKey = new RegExp('^(_|attachments)', 'i'),
         node_modules = [],
+        appPrefix = 'app::',
         appData = {
             libs: []
         };
+        
+    // if this is an application, then return it in the results
+    if (doc._id.slice(0, appPrefix.length) === appPrefix) {
+        if (doc._attachments) {
+            for (key in doc._attachments) {
+                if (reLib.test(key)) {
+                    appData.libs.push(key);
+                } // if
+            } // for
+        } // if
 
-    if (doc._attachments) {
-        for (key in doc._attachments) {
-            if (reLib.test(key)) {
-                appData.libs.push(key);
+        // update the appdata with relevant data
+        for (key in doc) {
+            if (! ignoreKey.test(key)) {
+                appData[key] = doc[key];
             } // if
         } // for
-    } // if
-    
-    // update the appdata with relevant data
-    for (key in doc) {
-        if (! ignoreKey.test(key)) {
-            appData[key] = doc[key];
-        } // if
-    } // for
-    
-    emit(doc.title || doc._id, appData);
+
+        appData.id = doc._id.slice(appPrefix.length);
+        emit(doc.title || doc._id, appData);
+    }
 }
 
 module.exports = {
     filters: {
-        valid_items: function(doc, req) {
-            return true;
+        apps: function(doc, req) {
+            var appPrefix = 'app::';
+
+            return doc._id.slice(0, appPrefix.length) === appPrefix;
         }
     },
     
