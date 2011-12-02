a33fe354e05cd09e66c425a929f0114bc30d9634
Damon Oehlman <damon.oehlman@gmail.com>
2011-12-02 19:38:51 +1000

    Added spawn helper, bump version (0.2.2)

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+var path = require('path'),
+    spawn = require('child_process').spawn;
+
 exports.init = require('./lib/mesh').init;
 exports.MeshApp = require('./lib/meshapp').MeshApp;
+
+exports.spawn = function() {
+    return spawn(
+        path.resolve(__dirname, 'bin/mesh'), 
+        Array.prototype.slice.call(arguments, 0)
+    );
+};
