562f42c323ccc0abc87b34a8efbe257c45c1d4ad
Damon Oehlman <damon.oehlman@sidelab.com>
2012-01-12 15:09:23 +1000

    Started rebuilding mesh tools

--- assets/templates/default/app.js
+++ assets/templates/default/app.js
@@ -1,27 +1,5 @@
 module.exports = {
     routes: [
-        '/time => test.getTime',
-        
-        {
-            path: '/counter',
-            handler: 'test.getCount'
-        },
-        
-        {
-            path: '/uuid',
-            handler: 'test.uuid'
-        },
-        
-        { 
-            path: '/html-test',
-            handler: 'test.genHTML'
-        }
-    ],
-    
-    jobs: {
-        'Test Job': {
-            pattern: '*/5 * * * * *',
-            handler: 'test.testJob'
-        }
-    }
+        '/ => test.hello'
+    ]
 };
